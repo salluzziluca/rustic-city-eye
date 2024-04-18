56087311b10682c9ff447f17ba608a4d7e86f8ee
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-04-18 17:47:26 -0300

    refactor: empezar a diagramar el scafolding

--- rustic_city_eye/src/lib.rs
+++ rustic_city_eye/src/lib.rs
@@ -1,3 +1,7 @@
+pub mod camera_system;
 #[doc = include_str!("doc/informe.md")]
 #[doc = include_str!("doc/marco_teorico.md")]
 pub mod doc;
+pub mod drone_system;
+pub mod monitoring_app;
+pub mod mqtt;
