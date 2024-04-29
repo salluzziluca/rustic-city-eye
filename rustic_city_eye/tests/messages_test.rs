15212abcab2f1978a252f3f00a9fdcf2b28afb00
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-04-29 01:02:28 -0300

    test: borrar tests de integracion

--- rustic_city_eye/tests/messages_test.rs
+++ rustic_city_eye/tests/messages_test.rs
@@ -1,11 +1,11 @@
-use rustic_city_eye::mqtt::client::server_run;
-use std::net::TcpStream;
+// use rustic_city_eye::mqtt::client::server_run;
+// use std::net::TcpStream;
 
-#[test]
-fn test_connect() {
-    let addrs = "127.0.0.1:5000";
-    Broker::server_run(addrs).unwrap();
-    let mut stream = TcpStream::connect(addrs).unwrap();
-    let connect = ClientMessage::Connect {};
-    connect.write_to(&mut stream).unwrap();
-}
+// #[test]
+// fn test_connect() {
+//     let addrs = "127.0.0.1:5000";
+//     Broker::server_run(addrs).unwrap();
+//     let mut stream = TcpStream::connect(addrs).unwrap();
+//     let connect = ClientMessage::Connect {};
+//     connect.write_to(&mut stream).unwrap();
+// }
