43e0400e062417111be4448e716b708beb809b60
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2024-05-03 10:34:57 -0300

    fixing bug

--- rustic_city_eye/src/mqtt/broker.rs
+++ rustic_city_eye/src/mqtt/broker.rs
@@ -49,7 +49,7 @@ impl Broker {
         Ok(())
     }
 
-    ///handles the different client messages, and it sends the corresponding acknoledges
+    ///Handles the different client messages, and it sends the corresponding acknoledges.
     fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
         while let Ok(message) = ClientMessage::read_from(stream) {
             match message {
@@ -90,7 +90,7 @@ impl Broker {
                     if qos == 1 {
                         println!("sending puback...");
                         let puback = BrokerMessage::Puback { packet_id_msb: packet_id_bytes[0], packet_id_lsb: packet_id_bytes[1], reason_code: 1 };
-                        puback.write_to(stream).unwrap();
+                        puback.write_to(stream)?;
                     }
                 }
             }
@@ -112,76 +112,8 @@ impl Broker {
 
 fn main() -> Result<(), ProtocolError> {
     let argv = args().collect::<Vec<String>>();
-    if argv.len() != SERVER_ARGS {
-        let app_name = &argv[0];
-        println!("Usage:\n{:?} <puerto>", app_name);
-        return Err(ProtocolError::InvalidNumberOfArguments);
-    }
-
-    let address = "127.0.0.1:".to_owned() + &argv[1];
-    server_run(&address).unwrap();
-    Ok(())
-}
-
-fn server_run(address: &str) -> std::io::Result<()> {
-    let listener = TcpListener::bind(address)?;
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                std::thread::spawn(move || handle_client(&mut stream));
-            }
-            Err(err) => return Err(err),
-        }
-    }
-    Ok(())
-}
-#[allow(dead_code)]
-fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
-    while let Ok(message) = ClientMessage::read_from(stream) {
-        match message {
-            ClientMessage::Connect {
-                clean_start: _,
-                last_will_flag: _,
-                last_will_qos: _,
-                last_will_retain: _,
-                username: _,
-                password: _,
-                keep_alive: _,
-                properties: _,
-                client_id: _,
-                will_properties: _,
-                last_will_topic: _,
-                last_will_message: _,
-            } => {
-                println!("Recibí un connect: {:?}", message);
-                let connack = BrokerMessage::Connack {
-                    //session_present: true,
-                    //return_code: 0,
-                };
-                println!("Sending connack: {:?}", connack);
-                connack.write_to(stream).unwrap();
-            }
-            ClientMessage::Publish {
-                packet_id,
-                topic_name: _,
-                qos,
-                retain_flag: _,
-                payload: _,
-                dup_flag: _,
-                properties: _,
-            } => {
-                println!("Recibí un publish: {:?}", message);
-                let packet_id_bytes: [u8; 2] = packet_id.to_be_bytes();
-
-                if qos == 1 {
-                    println!("sending puback...");
-                    let puback = BrokerMessage::Puback { packet_id_msb: packet_id_bytes[0], packet_id_lsb: packet_id_bytes[1], reason_code: 1 };
-                    puback.write_to(stream).unwrap();
-                }
-            }
-        }
-    }
+    let broker = Broker::build(argv)?;
+    let _ = broker.server_run();
     Ok(())
 }
 
