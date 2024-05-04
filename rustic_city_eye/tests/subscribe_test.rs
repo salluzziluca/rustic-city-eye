55f3023160b94073c1314e4d81b3ec7932008300
lihuencarranza <lcarranza@fi.uba.ar>
2024-05-04 18:41:14 -0300

    test: checking suback

--- rustic_city_eye/tests/subscribe_test.rs
+++ rustic_city_eye/tests/subscribe_test.rs
@@ -1,6 +1,7 @@
 use std::io::Cursor;
 
 use rustic_city_eye::mqtt::{client_message, subscribe_properties::SubscribeProperties};
+use rustic_city_eye::mqtt::broker_message;
 
 #[test]
 fn test_sub() {
@@ -19,4 +20,19 @@ fn test_sub() {
     cursor.set_position(0);
     let read_sub = client_message::ClientMessage::read_from(&mut cursor).unwrap();
     assert_eq!(sub, read_sub);
+}
+
+#[test]
+fn test_suback() {
+    let suback = broker_message::BrokerMessage::Suback {
+        reason_code: 1,
+        packet_id_msb: 1,
+        packet_id_lsb: 1,
+    };
+
+    let mut cursor = Cursor::new(Vec::<u8>::new());
+    suback.write_to(&mut cursor).unwrap();
+    cursor.set_position(0);
+    let read_suback = broker_message::BrokerMessage::read_from(&mut cursor).unwrap();
+    assert_eq!(suback, read_suback);
 }
