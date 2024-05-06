e0de3819c7181df838232c17ba3ec37b698296c5
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2024-05-06 17:01:29 -0300

    solving imports issue

--- rustic_city_eye/src/lib.rs
+++ rustic_city_eye/src/lib.rs
@@ -4,4 +4,9 @@ pub mod camera_system;
 pub mod doc;
 pub mod drone_system;
 pub mod monitoring_app;
-pub mod mqtt;
+pub mod mqtt {
+    pub mod client;
+    pub mod protocol_error;
+    pub mod connect_properties;
+    pub mod will_properties;
+}
