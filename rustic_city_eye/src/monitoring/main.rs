766521a713e2657ae914f1b55b1b86491a3d4391
ignaciodemarchi <ignacio22demarchi@gmail.com>
2024-06-02 17:07:55 -0300

    Added the map

--- rustic_city_eye/src/monitoring/main.rs
+++ rustic_city_eye/src/monitoring/main.rs
@@ -1,5 +1,3 @@
-use std::env;
-
 use eframe::{run_native, App, CreationContext, NativeOptions};
 use egui::{CentralPanel, Image, RichText, TextStyle};
 
@@ -8,11 +6,11 @@ struct MyApp {
     ip: String,
     port: String,
     connected: bool,
-    last_clicked_pos: egui::Pos2,
+    last_clicked_pos: egui::Pos2
 }
 
-impl MyApp {
-    fn handle_form(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+impl MyApp{
+    fn handle_form(&mut self,ctx: &eframe::egui::Context, _frame: &mut eframe::Frame){
         CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered(|ui| {
                 ui.label(
@@ -49,22 +47,40 @@ impl MyApp {
                 }
             })
         });
+
     }
+
+
 }
+
 impl App for MyApp {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         if !self.connected {
             self.handle_form(ctx, _frame);
         } else {
-            let image = egui::include_image!("Map.png");
             CentralPanel::default().show(ctx, |ui| {
-                ui.add(egui::Image::new(image));
+                ui.image(egui::include_image!("assets/Map.png"));
+
+                if ui.input(|input| input.pointer.primary_clicked()) {
+                    if let Some(pos) = ui.input(|input| input.pointer.interact_pos()){
+                        println!("Clicked at {:?}", pos);
+                        self.last_clicked_pos = pos;
+                    }
+                }
+                if  self.last_clicked_pos != egui::Pos2::ZERO {
+                    ui.painter().circle(self.last_clicked_pos, 10.0, egui::Color32::RED, egui::Stroke::new(1.0, egui::Color32::WHITE));
+                }
+
             });
+
         }
     }
+    
 }
 
 fn create_my_app(_cc: &CreationContext<'_>) -> Box<dyn App> {
+    egui_extras::install_image_loaders(&_cc.egui_ctx);
+
     Box::new(MyApp {
         usermane: String::new(),
         ip: String::new(),
