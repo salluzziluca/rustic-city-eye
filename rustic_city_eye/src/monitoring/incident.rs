f39f45a6dacbe4a42b6bd5b814c38c070f765183
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-06-03 19:40:37 -0300

    test: incident.rs

--- rustic_city_eye/src/monitoring/incident.rs
+++ rustic_city_eye/src/monitoring/incident.rs
@@ -39,3 +39,46 @@ impl Incident {
         Ok(())
     }
 }
+
+#[cfg(test)]
+
+mod tests {
+    use std::io::Cursor;
+
+    use super::*;
+    #[test]
+    fn test_new_incident() {
+        let location = Location::new("1.0".to_string(), "2.0".to_string());
+        let incident = Incident::new(location.clone());
+        assert_eq!(incident.get_location(), location);
+    }
+
+    #[test]
+    fn test_parse_to_string() {
+        let location = Location::new("1.0".to_string(), "2.0".to_string());
+        let incident = Incident::new(location.clone());
+
+        assert_eq!(incident.parse_to_string(), "Incident at (1, 2)");
+    }
+
+    #[test]
+    fn test_from_string() {
+        let location = Location::new("1.0".to_string(), "2.0".to_string());
+        let incident = Incident::new(location.clone());
+        assert_eq!(
+            Incident::from_string("Incident at 1, 2".to_string()),
+            incident
+        );
+    }
+
+    #[test]
+    fn test_write_to() {
+        let mut cursor = Cursor::new(Vec::<u8>::new());
+        let location = Location::new("1.0".to_string(), "2.0".to_string());
+        let incident = Incident::new(location.clone());
+
+        incident.write_to(&mut cursor).unwrap();
+        let buffer = cursor.into_inner();
+        assert_eq!(buffer, vec![0, 1, 50, 0, 1, 49]);
+    }
+}
