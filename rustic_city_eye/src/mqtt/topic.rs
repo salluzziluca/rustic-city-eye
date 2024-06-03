318d1328d2ef8434a4506af0bcffef47daa487ec
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-06-03 19:42:29 -0300

    test: topic.rs

--- rustic_city_eye/src/mqtt/topic.rs
+++ rustic_city_eye/src/mqtt/topic.rs
@@ -114,49 +114,4 @@ mod tests {
 
         Ok(())
     }
-
-    // #[test]
-    // fn test_02_adding_sub_ok() -> std::io::Result<()> {
-    //     let mut topic = Topic::new();
-
-    //     let (tx, _rx) = mpsc::channel();
-    //     topic.add_subscriber(tx);
-
-    //     Ok(())
-    // }
-
-    // #[test]
-    // fn test_03_adding_sub_and_sending_message_ok() {
-    //     let mut topic = Topic::new();
-    //     let (tx, rx) = mpsc::channel();
-    //     topic.add_subscriber(tx);
-
-    //     let _ = topic.send_message("holaholahola");
-
-    //     let message_received = rx.recv().unwrap();
-
-    //     assert_eq!(message_received, "holaholahola".to_string());
-    // }
-
-    // #[test]
-    // fn test_04_adding_multiple_subs_and_sending_message_ok() {
-    //     let mut topic = Topic::new();
-    //     let (tx1, rx1) = mpsc::channel();
-    //     let (tx2, rx2) = mpsc::channel();
-    //     let (tx3, rx3) = mpsc::channel();
-
-    //     topic.add_subscriber(tx1);
-    //     topic.add_subscriber(tx2);
-    //     topic.add_subscriber(tx3);
-
-    //     let _ = topic.send_message("holaholahola");
-
-    //     let message_received1 = rx1.recv().unwrap();
-    //     let message_received2 = rx2.recv().unwrap();
-    //     let message_received3 = rx3.recv().unwrap();
-
-    //     assert_eq!(message_received1, "holaholahola".to_string());
-    //     assert_eq!(message_received2, "holaholahola".to_string());
-    //     assert_eq!(message_received3, "holaholahola".to_string());
-    // }
 }
