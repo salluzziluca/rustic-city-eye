cef79caa7d4341fb504542444b820e4e7ef6a502
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2024-06-06 11:38:50 -0300

    feat: simulo la descarga de bateria del Drone.
    agrego el crate chrono, para simular la descarga de bateria.
    
    agrego algunas docs que me faltaban.

--- rustic_city_eye/src/drone_system/drone_config.rs
+++ rustic_city_eye/src/drone_system/drone_config.rs
@@ -1,13 +1,22 @@
+use chrono::Utc;
 use serde::Deserialize;
 
 use std::{fs::File, io::BufReader};
 
 use super::drone_error::DroneError;
 
+/// Sirve para levantar la configuracion del Drone a partir del JSON.
+/// Pone a correr al Drone:
+///     - Simula su descarga de bateria.
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct DroneConfig {
     /// Indica el nivel de bateria del Drone. Se va descargando con el paso del tiempo.
-    battery_level: usize,
+    battery_level: i64,
+
+    /// Indice la tasa de desgaste de la bateria en milisegundos.
+    /// Por ej: si vale 10, por cada 10 segundos que pasen, la
+    /// bateria del Drone se reducira en un 1 por ciento.
+    battery_discharge_rate_milisecs: i64,
 
     /// El Drone circulara en un area de operacion determinado por el archivo de configuracion.
     /// A medida que pasa el tiempo, el Drone va moviendose dentro de ese area.
@@ -15,6 +24,7 @@ pub struct DroneConfig {
 }
 
 impl DroneConfig {
+    /// Toma un path a un archivo de configuracion y levanta el DroneConfig.
     pub fn read_drone_config(file_path: &str) -> Result<DroneConfig, DroneError> {
         let config_file = match File::open(file_path) {
             Ok(file) => file,
@@ -27,9 +37,31 @@ impl DroneConfig {
             Err(_) => return Err(DroneError::ReadingConfigFileError),
         };
 
-        println!("config {:?}", config);
-
         Ok(config)
+    }
+
+    /// Simula la descarga de bateria del Drone, dependiendo de su
+    /// tasa de descarga en milisegundos.
+    pub fn run_drone(&mut self) {
+        let mut start_time = Utc::now();
+
+        while self.battery_level > 0 {
+            let current_time = Utc::now();
+            let elapsed = current_time
+                .signed_duration_since(start_time)
+                .num_milliseconds();
+
+            if elapsed >= self.battery_discharge_rate_milisecs {
+                self.battery_level -= 1;
+
+                if self.battery_level < 0 {
+                    self.battery_level = 0;
+                    break;
+                }
+
+                start_time = current_time;
+            }
+        }
     }
 }
 
@@ -45,6 +77,7 @@ mod tests {
         assert_eq!(
             DroneConfig {
                 battery_level: 100,
+                battery_discharge_rate_milisecs: 50,
                 operation_radius: 3
             },
             config
@@ -56,5 +89,13 @@ mod tests {
         let config = DroneConfig::read_drone_config("este/es/un/path/feo");
 
         assert!(config.is_err());
+    }
+
+    #[test]
+    fn test_03_running_drone_ok() {
+        let mut config =
+            DroneConfig::read_drone_config("./src/drone_system/drone_config.json").unwrap();
+
+        config.run_drone();
     }
 }
