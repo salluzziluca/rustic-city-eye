325802ff9513725844d76b490e938249724afa51
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-06-11 17:25:25 -0300

    test: agregar test de desserializacion

--- rustic_city_eye/src/mqtt/connect/connect_config.rs
+++ rustic_city_eye/src/mqtt/connect/connect_config.rs
@@ -48,6 +48,35 @@ impl MessagesConfig for ConnectConfig {
 }
 
 impl ConnectConfig {
+    pub fn new(
+        clean_start: bool,
+        last_will_flag: bool,
+        last_will_qos: u8,
+        last_will_retain: bool,
+        keep_alive: u16,
+        properties: ConnectProperties,
+        client_id: String,
+        will_properties: WillProperties,
+        last_will_topic: String,
+        last_will_message: String,
+        username: String,
+        password: String,
+    ) -> ConnectConfig {
+        ConnectConfig {
+            clean_start,
+            last_will_flag,
+            last_will_qos,
+            last_will_retain,
+            keep_alive,
+            properties,
+            client_id,
+            will_properties: Some(will_properties),
+            last_will_topic: Some(last_will_topic),
+            last_will_message: Some(last_will_message),
+            username: Some(username),
+            password: Some(password.into_bytes()),
+        }
+    }
     /// Abre un archivo de configuracion con propiedades y guarda sus lecturas.
     pub fn read_connect_config(file_path: &str) -> Result<ConnectConfig, ProtocolError> {
         let config_file = match File::open(file_path) {
@@ -204,11 +233,7 @@ impl ConnectConfig {
         })
     }
 }
-#[allow(dead_code)]
-// fn read_json_to_connect_config(json_data: &str) -> Result<ConnectConfig, Box<dyn Error>> {
-//     let connect_config: ConnectConfig = serde_json::from_str(json_data)?;
-//     Ok(connect_config)
-// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -241,151 +266,140 @@ mod tests {
         assert!(config_err.is_err());
     }
 
-    // #[test]
-    // fn test_read_json_connect_config() {
-    //     let json_data = r#"{
-    //         "clean_start": true,
-    //         "last_will_flag": true,
-    //         "last_will_qos": 1,
-    //         "last_will_retain": true,
-    //         "keep_alive": 35,
-    //         "properties": {
-    //             "session_expiry_interval": 30,
-    //             "receive_maximum": 1,
-    //             "maximum_packet_size": 20,
-    //             "topic_alias_maximum": 20,
-    //             "request_response_information": true,
-    //             "request_problem_information": true,
-    //             "user_properties": [
-    //                 [
-    //                     "hola",
-    //                     "chau"
-    //                 ]
-    //             ],
-    //             "authentication_method": "password-based",
-    //             "authentication_data": [
-    //                 1,
-    //                 2,
-    //                 3
-    //             ]
-    //         },
-    //         "client_id": "juancito",
-    //         "will_properties": {
-    //             "last_will_delay_interval": 1,
-    //             "payload_format_indicator": 1,
-    //             "message_expiry_interval": 1,
-    //             "content_type": "a",
-    //             "response_topic": "a",
-    //             "correlation_data": [
-    //                 1,
-    //                 2,
-    //                 3
-    //             ],
-    //             "user_properties": [
-    //                 [
-    //                     "a",
-    //                     "a"
-    //                 ]
-    //             ]
-    //         },
-    //         "last_will_topic": "camera system",
-    //         "last_will_message": "soy el monitoring y me desconecte",
-    //         "username": "a",
-    //         "password": "a"
-    //     }"#;
-    //     let connect_config = read_json_to_connect_config(json_data).unwrap();
-    //     let expected_connect_config = ConnectConfig::new(
-    //         true,
-    //         true,
-    //         1,
-    //         true,
-    //         35,
-    //         ConnectProperties::new(
-    //             30,
-    //             1,
-    //             20,
-    //             20,
-    //             true,
-    //             true,
-    //             vec![("hola".to_string(), "chau".to_string())],
-    //             "password-based".to_string(),
-    //             vec![1, 2, 3],
-    //         ),
-    //         "juancito".to_string(),
-    //         Some(WillProperties::new(
-    //             1,
-    //             1,
-    //             1,
-    //             "a".to_string(),
-    //             "a".to_string(),
-    //             [1, 2, 3].to_vec(),
-    //             vec![("a".to_string(), "a".to_string())],
-    //         )),
-    //         Some("camera system".to_string()),
-    //         Some("soy el monitoring y me desconecte".to_string()),
-    //         "a".to_string(),
-    //         "a".to_string(),
-    //     );
-    //     assert_eq!(connect_config, expected_connect_config);
-    // }
+    #[test]
+    fn test_read_json_connect_config() {
+        let json_data = r#"{
+            "clean_start": true,
+            "last_will_flag": true,
+            "last_will_qos": 1,
+            "last_will_retain": true,
+            "keep_alive": 35,
+            "properties": {
+                "session_expiry_interval": 30,
+                "receive_maximum": 1,
+                "maximum_packet_size": 20,
+                "topic_alias_maximum": 20,
+                "request_response_information": true,
+                "request_problem_information": true,
+                "user_properties": [
+                    [
+                        "hola",
+                        "chau"
+                    ]
+                ],
+                "authentication_method": "password-based",
+                "authentication_data": [
+                    1,
+                    2,
+                    3
+                ]
+            },
+            "client_id": "juancito",
+            "will_properties": {
+                "last_will_delay_interval": 1,
+                "payload_format_indicator": 1,
+                "message_expiry_interval": 1,
+                "content_type": "a",
+                "response_topic": "a",
+                "correlation_data": [
+                    1,
+                    2,
+                    3
+                ],
+                "user_properties": [
+                    [
+                        "a",
+                        "a"
+                    ]
+                ]
+            },
+            "last_will_topic": "camera system",
+            "last_will_message": "soy el monitoring y me desconecte",
+            "username": "a",
+            "password": "a"
+        }"#;
+        let connect_config = ConnectConfig::read_connect_config(json_data).unwrap();
+        let expected_connect_config = ConnectConfig::new(
+            true,
+            true,
+            1,
+            true,
+            35,
+            ConnectProperties::new(
+                30,
+                1,
+                20,
+                20,
+                true,
+                true,
+                vec![("hola".to_string(), "chau".to_string())],
+                "password-based".to_string(),
+                vec![1, 2, 3],
+            ),
+            "juancito".to_string(),
+            WillProperties::new(
+                1,
+                1,
+                1,
+                "a".to_string(),
+                "a".to_string(),
+                [1, 2, 3].to_vec(),
+                vec![("a".to_string(), "a".to_string())],
+            ),
+            "camera system".to_string(),
+            "soy el monitoring y me desconecte".to_string(),
+            "a".to_string(),
+            "a".to_string(),
+        );
+        assert_eq!(connect_config, expected_connect_config);
+    }
 
-    // #[test]
-    // fn test_parse_message() {
-    //     let connect_properties = ConnectProperties::new(
-    //         30,
-    //         1,
-    //         20,
-    //         20,
-    //         true,
-    //         true,
-    //         vec![("hola".to_string(), "chau".to_string())],
-    //         "auth".to_string(),
-    //         vec![1, 2, 3],
-    //     );
+    #[test]
+    fn test_parse_message() {
+        let connect_properties = ConnectProperties::new(
+            30,
+            1,
+            20,
+            20,
+            true,
+            true,
+            vec![("hola".to_string(), "chau".to_string())],
+            "auth".to_string(),
+            vec![1, 2, 3],
+        );
 
-    //     let will_properties = WillProperties::new(
-    //         1,
-    //         1,
-    //         1,
-    //         "a".to_string(),
-    //         "a".to_string(),
-    //         [1, 2, 3].to_vec(),
-    //         vec![("a".to_string(), "a".to_string())],
-    //     );
+        let will_properties = WillProperties::new(
+            1,
+            1,
+            1,
+            "a".to_string(),
+            "a".to_string(),
+            [1, 2, 3].to_vec(),
+            vec![("a".to_string(), "a".to_string())],
+        );
 
-    //     let connect_config = ConnectConfig::new(
-    //         true,
-    //         true,
-    //         1,
-    //         true,
-    //         35,
-    //         connect_properties.clone(),
-    //         "juancito".to_string(),
-    //         will_properties.clone(),
-    //         "camera system".to_string(),
-    //         "soy el monitoring y me desconecte".to_string(),
-    //         "a".to_string(),
-    //         "a".to_string(),
-    //     );
+        let connect_config = ConnectConfig::new(
+            true,
+            true,
+            1,
+            true,
+            35,
+            connect_properties.clone(),
+            "juancito".to_string(),
+            will_properties.clone(),
+            "camera system".to_string(),
+            "soy el monitoring y me desconecte".to_string(),
+            "a".to_string(),
+            "a".to_string(),
+        );
 
-    //     let connect_message = connect_config.parse_message(1);
+        let connect_message = connect_config.parse_message(1);
 
-    //     assert_eq!(
-    //         connect_message,
-    //         ClientMessage::Connect {
-    //             clean_start: true,
-    //             last_will_flag: true,
-    //             last_will_qos: 1,
-    //             last_will_retain: true,
-    //             keep_alive: 35,
-    //             properties: connect_properties,
-    //             client_id: "juancito".to_string(),
-    //             will_properties,
-    //             last_will_topic: "camera system".to_string(),
-    //             last_will_message: "soy el monitoring y me desconecte".to_string(),
-    //             username: "a".to_string(),
-    //             password: "a".to_string(),
-    //         }
-    //     );
-    // }
+        assert_eq!(
+            connect_message,
+            ClientMessage::Connect {
+                connect_config: connect_config.clone()
+            }
+        );
+    }
 }
