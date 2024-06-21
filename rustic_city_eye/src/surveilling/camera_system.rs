be1f8b468b66dfaf4bc74959b9248dd83f7fc24c
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-06-21 16:32:34 -0300

    fix: arreglar bug en desactivacion por cercania

--- rustic_city_eye/src/surveilling/camera_system.rs
+++ rustic_city_eye/src/surveilling/camera_system.rs
@@ -152,15 +152,20 @@ impl<T: ClientTrait + Clone> CameraSystem<T> {
                         payload: PayloadTypes::IncidentLocation(payload),
                         ..
                     }) => {
-                        if topic_name != "accidente" {
+                        if topic_name == "accidente" {
+                            let location = payload.get_incident().get_location();
+                            drop(lock); // Release the lock here
+                            self.activate_cameras(location)
+                                .map_err(|e| ProtocolError::CameraError(e.to_string()))?;
+
+                            continue;
+                        } else if topic_name == "accidenteresuelto" {
+                            let location = payload.get_incident().get_location();
+                            drop(lock); // Release the lock here
+                            self.deactivate_cameras(location)
+                                .map_err(|e| ProtocolError::CameraError(e.to_string()))?;
                             continue;
                         }
-                        let location = payload.get_incident().get_location();
-                        drop(lock); // Release the lock here
-                        self.activate_cameras(location)
-                            .map_err(|e| ProtocolError::CameraError(e.to_string()))?;
-
-                        continue;
                     }
                     Ok(_) => continue, // Handle other message types if necessary
                     Err(e) => {
@@ -216,6 +221,53 @@ impl<T: ClientTrait + Clone> CameraSystem<T> {
         // Activate cameras by the collected locations
         for loc in locations_to_activate {
             self.activate_cameras_by_camera_location(loc)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn deactivate_cameras(&mut self, location: Location) -> Result<(), CameraError> {
+        // Collect the locations that need to be activated first
+        let locations_to_activate: Vec<Location> = self
+            .cameras
+            .values_mut()
+            .filter_map(|camera| {
+                let distancia = camera.get_location().distance(location.clone());
+                if distancia <= AREA_DE_ALCANCE {
+                    camera.set_sleep_mode(true);
+                    Some(camera.get_location())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        // Activate cameras by the collected locations
+        for loc in locations_to_activate {
+            self.deactivate_cameras_by_camera_location(loc)?;
+        }
+
+        Ok(())
+    }
+
+    fn deactivate_cameras_by_camera_location(
+        &mut self,
+        location: Location,
+    ) -> Result<(), CameraError> {
+        // Collect the locations that need to be activated first
+        let mut locations_to_activate = Vec::new();
+
+        for camera in self.cameras.values_mut() {
+            let distancia = camera.get_location().distance(location.clone());
+            if distancia <= NIVEL_DE_PROXIMIDAD_MAXIMO && !camera.get_sleep_mode() {
+                camera.set_sleep_mode(true);
+                locations_to_activate.push(camera.get_location());
+            }
+        }
+
+        // Activate cameras by the collected locations
+        for loc in locations_to_activate {
+            self.deactivate_cameras_by_camera_location(loc)?;
         }
 
         Ok(())
@@ -551,7 +603,208 @@ mod tests {
     }
 
     #[test]
+    fn test09_desactivar_camara() {
+        let args = vec!["127.0.0.1".to_string(), "5010".to_string()];
+        let mut broker = match Broker::new(args) {
+            Ok(broker) => broker,
+            Err(e) => panic!("Error creating broker: {:?}", e),
+        };
 
+        let server_ready = Arc::new((Mutex::new(false), Condvar::new()));
+        let server_ready_clone = server_ready.clone();
+        thread::spawn(move || {
+            {
+                let (lock, cvar) = &*server_ready_clone;
+                let mut ready = lock.lock().unwrap();
+                *ready = true;
+                cvar.notify_all();
+            }
+            let _ = broker.server_run();
+        });
+
+        // Wait for the server to start
+        {
+            let (lock, cvar) = &*server_ready;
+            let mut ready = lock.lock().unwrap();
+            while !*ready {
+                ready = cvar.wait(ready).unwrap();
+            }
+        }
+        let addr = "127.0.0.1:5010";
+        let handle = thread::spawn(move || {
+            let mut camera_system =
+                CameraSystem::<Client>::with_real_client(addr.to_string()).unwrap();
+            let location = Location::new(1.0, 2.0);
+            let id = camera_system.add_camera(location.clone()).unwrap();
+            assert_eq!(camera_system.get_cameras().len(), 1);
+            assert_eq!(
+                camera_system.get_camera_by_id(id).unwrap().get_location(),
+                location
+            );
+            let incident_location = Location::new(1.0, 2.0);
+            camera_system
+                .activate_cameras(incident_location.clone())
+                .unwrap();
+            for camera in camera_system.get_cameras().values() {
+                assert!(!camera.get_sleep_mode());
+            }
+            camera_system.deactivate_cameras(incident_location).unwrap();
+            for camera in camera_system.get_cameras().values() {
+                assert!(camera.get_sleep_mode());
+            }
+        });
+        handle.join().unwrap();
+    }
+
+    #[test]
+    fn test_10_deactivate_multiple_cameras() {
+        let args = vec!["127.0.0.1".to_string(), "5011".to_string()];
+        let mut broker = match Broker::new(args) {
+            Ok(broker) => broker,
+            Err(e) => panic!("Error creating broker: {:?}", e),
+        };
+
+        let server_ready = Arc::new((Mutex::new(false), Condvar::new()));
+        let server_ready_clone = server_ready.clone();
+        thread::spawn(move || {
+            {
+                let (lock, cvar) = &*server_ready_clone;
+                let mut ready = lock.lock().unwrap();
+                *ready = true;
+                cvar.notify_all();
+            }
+            let _ = broker.server_run();
+        });
+
+        // Wait for the server to start
+        {
+            let (lock, cvar) = &*server_ready;
+            let mut ready = lock.lock().unwrap();
+            while !*ready {
+                ready = cvar.wait(ready).unwrap();
+            }
+        }
+        let addr = "127.0.0.1:5011";
+        let handle = thread::spawn(move || {
+            let mut camera_system =
+                CameraSystem::<Client>::with_real_client(addr.to_string()).unwrap();
+            let location = Location::new(5.0, 20.0);
+            let id = camera_system.add_camera(location.clone()).unwrap();
+            let location2 = Location::new(5.0, 2.0);
+            let id2 = camera_system.add_camera(location2.clone()).unwrap();
+            let location3 = Location::new(10.0, 2.0);
+            let id3 = camera_system.add_camera(location3.clone()).unwrap();
+            let location4 = Location::new(10.0, 20.0);
+            let id4 = camera_system.add_camera(location4.clone()).unwrap();
+            let location5 = Location::new(1.0, 2.0);
+            let id5 = camera_system.add_camera(location5.clone()).unwrap();
+            assert_eq!(camera_system.get_cameras().len(), 5);
+            assert_eq!(
+                camera_system.get_camera_by_id(id).unwrap().get_location(),
+                location
+            );
+            let incident_location = Location::new(1.0, 2.0);
+            camera_system
+                .activate_cameras(incident_location.clone())
+                .unwrap();
+            assert!(camera_system.get_camera_by_id(id).unwrap().get_sleep_mode());
+            assert!(!camera_system
+                .get_camera_by_id(id2)
+                .unwrap()
+                .get_sleep_mode());
+            assert!(!camera_system
+                .get_camera_by_id(id3)
+                .unwrap()
+                .get_sleep_mode());
+            assert!(camera_system
+                .get_camera_by_id(id4)
+                .unwrap()
+                .get_sleep_mode());
+            assert!(!camera_system
+                .get_camera_by_id(id5)
+                .unwrap()
+                .get_sleep_mode());
+
+            camera_system.deactivate_cameras(incident_location).unwrap();
+            assert!(camera_system.get_camera_by_id(id).unwrap().get_sleep_mode());
+            assert!(camera_system
+                .get_camera_by_id(id2)
+                .unwrap()
+                .get_sleep_mode());
+            assert!(camera_system
+                .get_camera_by_id(id3)
+                .unwrap()
+                .get_sleep_mode());
+            assert!(camera_system
+                .get_camera_by_id(id4)
+                .unwrap()
+                .get_sleep_mode());
+            assert!(camera_system
+                .get_camera_by_id(id5)
+                .unwrap()
+                .get_sleep_mode());
+        });
+        handle.join().unwrap();
+    }
+
+    #[test]
+    fn test_11_desactivar_camara_por_proximidad() {
+        let args = vec!["127.0.0.1".to_string(), "5012".to_string()];
+        let mut broker = match Broker::new(args) {
+            Ok(broker) => broker,
+            Err(e) => panic!("Error creating broker: {:?}", e),
+        };
+
+        let server_ready = Arc::new((Mutex::new(false), Condvar::new()));
+        let server_ready_clone = server_ready.clone();
+        thread::spawn(move || {
+            {
+                let (lock, cvar) = &*server_ready_clone;
+                let mut ready = lock.lock().unwrap();
+                *ready = true;
+                cvar.notify_all();
+            }
+            let _ = broker.server_run();
+        });
+
+        // Wait for the server to start
+        {
+            let (lock, cvar) = &*server_ready;
+            let mut ready = lock.lock().unwrap();
+            while !*ready {
+                ready = cvar.wait(ready).unwrap();
+            }
+        }
+        let addr = "127.0.0.1:5012";
+        let handle = thread::spawn(move || {
+            let mut camera_system =
+                CameraSystem::<Client>::with_real_client(addr.to_string()).unwrap();
+            let location = Location::new(10.0, 0.0);
+            let id = camera_system.add_camera(location.clone()).unwrap();
+            let location2 = Location::new(11.0, 0.0);
+            let id2 = camera_system.add_camera(location2.clone()).unwrap();
+
+            assert_eq!(camera_system.get_cameras().len(), 2);
+
+            let incident_location = Location::new(0.0, 0.0);
+            camera_system.activate_cameras(incident_location).unwrap();
+            assert!(!camera_system.get_camera_by_id(id).unwrap().get_sleep_mode());
+            assert!(!camera_system
+                .get_camera_by_id(id2)
+                .unwrap()
+                .get_sleep_mode());
+
+            let incident_location = Location::new(0.0, 0.0);
+            camera_system.deactivate_cameras(incident_location).unwrap();
+            assert!(camera_system.get_camera_by_id(id).unwrap().get_sleep_mode());
+            assert!(camera_system
+                .get_camera_by_id(id2)
+                .unwrap()
+                .get_sleep_mode());
+        });
+        handle.join().unwrap();
+    }
+    #[test]
     fn test_run_client() {
         #[derive(Debug, Clone)]
         pub struct MockClient {
