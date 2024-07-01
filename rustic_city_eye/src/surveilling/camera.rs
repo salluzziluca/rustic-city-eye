0543983dab89850b89d9055279520635cc178849
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-07-01 17:02:38 -0300

    refactor: no more unwraps

--- rustic_city_eye/src/surveilling/camera.rs
+++ rustic_city_eye/src/surveilling/camera.rs
@@ -41,12 +41,12 @@ impl Camera {
         self.sleep_mode = sleep_mode;
     }
     pub fn write_to(&mut self, stream: &mut dyn Write) -> Result<(), CameraError> {
-        let _ = write_u32(stream, &self.id).map_err(|_| CameraError::WriteError);
-        let _ = write_string(stream, &self.location.get_latitude().to_string())
-            .map_err(|_| CameraError::WriteError);
-        let _ = write_string(stream, &self.location.get_longitude().to_string())
-            .map_err(|_| CameraError::WriteError);
-        let _ = write_bool(stream, &self.sleep_mode).map_err(|_| CameraError::WriteError);
+        write_u32(stream, &self.id).map_err(|_| CameraError::WriteError)?;
+        write_string(stream, &self.location.get_latitude().to_string())
+            .map_err(|_| CameraError::WriteError)?;
+        write_string(stream, &self.location.get_longitude().to_string())
+            .map_err(|_| CameraError::WriteError)?;
+        write_bool(stream, &self.sleep_mode).map_err(|_| CameraError::WriteError)?;
 
         Ok(())
     }
