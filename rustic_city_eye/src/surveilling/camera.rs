e04723c5ff6b533e23e56beafd649ea1c9953ce9
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-07-07 14:45:48 -0300

    feat: cuando se cree una camara se cree su dir

--- rustic_city_eye/src/surveilling/camera.rs
+++ rustic_city_eye/src/surveilling/camera.rs
@@ -1,4 +1,8 @@
-use std::io::{Error, Read, Write};
+use std::{
+    fs,
+    io::{Error, Read, Write},
+    path::Path,
+};
 
 use serde::Deserialize;
 
@@ -12,7 +16,6 @@ use super::camera_error::CameraError;
 use crate::utils::writer::{write_bool, write_string, write_u32};
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
-#[allow(dead_code)]
 pub struct Camera {
     location: Location,
     id: u32,
@@ -21,11 +24,22 @@ pub struct Camera {
 
 impl Camera {
     pub fn new(location: Location, id: u32) -> Camera {
-        Self {
+        let camera = Self {
             location,
             id,
             sleep_mode: true,
+        };
+
+        let dir_name = format!("./{}", camera.id);
+        let path = "src/surveilling/cameras".to_string() + &dir_name;
+        if let Err(e) = fs::create_dir_all(Path::new(path.as_str())) {
+            eprintln!(
+                "Error al crear el directorio para la cámara {}: {}",
+                camera.id, e
+            );
         }
+
+        camera
     }
 
     pub fn get_location(&self) -> Location {
@@ -102,5 +116,14 @@ mod tests {
         let mut buffer = &buffer[..];
         let camera_read = Camera::read_from(&mut buffer).unwrap();
         assert_eq!(camera, camera_read);
+    }
+
+    #[test]
+    fn test_check_dir_creation() {
+        let location = Location::new(1.0, 2.0);
+        let camera = Camera::new(location, 1);
+        let dir_name = format!("./{}", camera.get_id());
+        let path = "src/surveilling/cameras".to_string() + &dir_name;
+        assert!(Path::new(path.as_str()).exists());
     }
 }
